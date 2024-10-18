c76bb053d6d9c49cc902fab3f233a63e6665a59d
bidianqing <bidianqing@qq.com>
2024-10-18 15:06:20 +0800

    *

--- AspNetCoreUseQuartzNet/Controllers/HomeController.cs
+++ AspNetCoreUseQuartzNet/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace AspNetCoreUseQuartzNet.Controllers
             ITrigger trigger = TriggerBuilder.Create()
                         .ForJob(HelloJob.Key)
                         .UsingJobData("name", name)
-                        .StartAt(DateTime.Now.AddSeconds(10))
+                        .StartAt(DateTime.Now.AddSeconds(-10))
                         .WithSimpleSchedule(x => x.WithIntervalInMinutes(3).RepeatForever())
                         .Build();
             //*/
@@ -82,13 +82,6 @@ namespace AspNetCoreUseQuartzNet.Controllers
             var scheduler = await _schedulerFactory.GetScheduler();
             await scheduler.ScheduleJob(trigger);
 
-            // 立即执行一个Job
-            //await scheduler.TriggerJob(HelloJob.Key);
-
-            // 停止调度某个触发器
-            //await scheduler.UnscheduleJob(trigger.Key);
-
-
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -96,6 +89,57 @@ namespace AspNetCoreUseQuartzNet.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        /// <summary>
+        /// 暂停某个触发器
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("PauseTrigger")]
+        public async Task PauseTrigger([FromQuery] string name)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+
+            await scheduler.PauseTrigger(new TriggerKey(name));
+        }
+
+        /// <summary>
+        /// 恢复某个触发器
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("ResumeTrigger")]
+        public async Task ResumeTrigger([FromQuery] string name)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+
+            await scheduler.ResumeTrigger(new TriggerKey(name));
+        }
+
+        /// <summary>
+        /// 卸载某个触发器
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("UnscheduleJob")]
+        public async Task UnscheduleJob([FromQuery] string name)
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+
+            await scheduler.UnscheduleJob(new TriggerKey(name));
+        }
+
+        /// <summary>
+        /// 立即执行一个Job
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("TriggerJob")]
+        public async Task TriggerJob()
+        {
+            var scheduler = await _schedulerFactory.GetScheduler();
+
+            await scheduler.TriggerJob(HelloJob.Key);
         }
     }
 }
